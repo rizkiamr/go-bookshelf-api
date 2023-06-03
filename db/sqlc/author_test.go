924ff615bb328fdc51e8e4af6cb0b76efb4bcf64
Muhammad Rizqy Amrullah <rizkii.amr@gmail.com>
2023-06-03 23:05:05 +0700

    feat: add unit test for author records

--- db/sqlc/author_test.go
+++ db/sqlc/author_test.go
@@ -3,12 +3,14 @@ package db
 import (
 	"context"
 	"testing"
+	"time"
 
+	"github.com/rizkiamr/go-bookshelf-api/util"
 	"github.com/stretchr/testify/require"
 )
 
-func TestCreateAuthor(t *testing.T) {
-	name := "penulis1"
+func createRandomAuthor(t *testing.T) Author {
+	name := util.RandomName()
 
 	author, err := testQueries.CreateAuthor(context.Background(), name)
 	require.NoError(t, err)
@@ -18,4 +20,23 @@ func TestCreateAuthor(t *testing.T) {
 
 	require.NotZero(t, author.ID)
 	require.NotZero(t, author.InsertedAt)
+
+	return author
+}
+
+func TestCreateAuthor(t *testing.T) {
+	createRandomAuthor(t)
+}
+
+func TestGetAuthor(t *testing.T) {
+	author1 := createRandomAuthor(t)
+	author2, err := testQueries.GetAuthor(context.Background(), author1.ID)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, author2)
+
+	require.Equal(t, author1.ID, author2.ID)
+	require.Equal(t, author1.Name, author2.Name)
+	
+	require.WithinDuration(t, author1.InsertedAt, author2.InsertedAt, time.Second)
 }
